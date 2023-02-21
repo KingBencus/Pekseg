92c9a64eb7cae8a6473637ebc00448572b2eae23
KingBencus <112863336+KingBencus@users.noreply.github.com>
2023-02-21 11:59:52 +0100

    Regisztráció mysql-el

---
+++ bejelentkezes-regisztracio/php/singup.php
@@ -0,0 +1,44 @@
+<?php
+
+if(isset($_POST['fname']) &&
+   isset($_POST['uname']) && 
+   isset($_POST['pass'])) {
+
+    include "../db_conn.php";
+
+    $fname = $_POST['fname'];
+    $uname = $_POST['uname'];
+    $pass = $_POST['pass'];
+
+    $data = "fname=".$fname."&uname=".$uname;
+
+
+    if (empty($fname)) {
+        $em = "Teljes név kötelező";
+        header("Location: ../register.php?error=$em&$data");
+        exit;
+    }else if(empty($uname)){
+        $em = "Felhasználó név kötelező";
+        header("Location: ../register.php?error=$em&$data");
+        exit;
+    }else if(empty($pass)){
+        $em = "Jelszó kötelező";
+        header("Location: ../register.php?error=$em&$data");
+        exit;
+    }else {
+
+        $pass = password_hash($pass, PASSWORD_DEFAULT);
+
+        $sql = "INSERT INTO users(fname, username, password)
+                VALUES(?,?,?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([$fname, $uname, $pass]);
+
+        header("Location: ../register.php?success=Sikeres regisztáció");
+        exit;
+    }
+
+}else {
+    header("Location: ../register.php?error=error");
+    exit;
+}
--- bejelentkezes-regisztracio/register.php
+++ bejelentkezes-regisztracio/register.php
@@ -6,37 +6,54 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Regisztráció</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="bejelentkezes-regisztracio/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-    <div class="d-flex justify-content-center align-items-center vh-100">
-        <form class="shadow w-450 p-3" 
-              action="regut.php"
+    <div class="d-flex justify-content-center align-items-center vh-100">  
+    
+    <form class="shadow w-450 p-3"
+              action="php/singup.php"
               method="post">
 
-                <h4 class="display-4 fs-1">Fiók létrehozása</h4><br>
+                <h4 class="display-4 text-center fs-1">Regisztráció</h4><br>
+                <?php if(isset($_GET['error'])){ ?>
+                <div class="alert alert-danger" role="alert">
+                   <?php echo $_GET['error']; ?> 
+                </div>
+                <?php } ?>
+
+                <?php if(isset($_GET['success'])){ ?>
+                <div class="alert alert-success" role="alert">
+                   <?php echo $_GET['success']; ?> 
+                </div>
+                <?php } ?>
+
             <div class="mb-3">
-                <label class="form-label"name="fnmae">Teljes név</label>
-                <input type="text"
+                <label class="form-label">Teljes név</label>
+                <input type="text" 
                        class="form-control"
-                       name="fname">
+                       name="fname"
+                       value="<?php echo (isset($_GET['fname']))?$_GET['fname']:
+                        "" ?>">
             </div>
 
             <div class="mb-3">
                 <label class="form-label">Felhasználó név</label>
-                <input type="text"
+                <input type="text" 
                        class="form-control"
-                       name="uname">
+                       name="uname"
+                       value="<?php echo (isset($_GET['uname']))?$_GET['uname']:
+                        "" ?>">
             </div>
 
             <div class="mb-3">
                 <label class="form-label">Jelszó</label>
-                <input type="text" 
+                <input type="password"
                        class="form-control"
                        name="pass">
             </div>
 
-            <button type="submit" class="btn btn-primary">Regisztrál</label></button>
+            <button type="submit" class="btn btn-primary">Regisztrál</button>
             <a href="login.php" class="link-secondary">Bejelentkezes</a>
         </form>
     </div>
